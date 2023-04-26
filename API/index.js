49bbaadb5c5c615f92a6aefb561b399c6f713fac
Siddhi Dhamnaskar <siddhidhamnaskar64@gmail.com>
2023-04-26 07:33:59 +0530

    done

--- API/index.js
+++ API/index.js
@@ -67,7 +67,8 @@ app.post("/login",async(req,res)=>{
         jwt.sign({Name:user.Name,Email:user.Email,id:user._id},secret,{},(err,token)=>{
             if(err) throw err;
             console.log(token);
-            res.cookie('token',token).json("ok")
+            
+            res.cookie('token',token).json(token)
         
         })
        }
@@ -89,9 +90,9 @@ app.post("/login",async(req,res)=>{
       }
 })
 
-app.get('/profile',async(req,res)=>{
+app.post('/profile',async(req,res)=>{
   try{
-    const {token}=req.cookies
+    let token=req.body.token;
     jwt.verify(token ,secret,{},(err,info)=>{
         if(err) throw err;
         res.json(info);
