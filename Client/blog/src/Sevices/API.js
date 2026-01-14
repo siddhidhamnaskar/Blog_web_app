20890c56a0f8b2d581ac3cecf45722b70458e345
Siddhi Dhamnaskar <112746473+siddhidhamnaskar@users.noreply.github.com>
2026-01-14 14:56:38 +0530

    Update base_url to new API endpoint

--- Client/blog/src/Sevices/API.js
+++ Client/blog/src/Sevices/API.js
@@ -1,4 +1,4 @@
-export const base_url="https://blogwebapp-production-6240.up.railway.app"
+export const base_url="https://blog-app-api-asc5.onrender.com"
 // export const base_url="http://localhost:3046"
 
 export const likePost = async (postId, token) => {
